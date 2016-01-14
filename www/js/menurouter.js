c6002143de0c163ff3c56a23f6bb7e5c5075f658
staticTJO <sk8terboy6931@hotmail.com>
2016-01-14 13:58:31 -0600

    Added Popover functionality, and dynamic changing status bar

--- www/js/menurouter.js
+++ www/js/menurouter.js
@@ -2,12 +2,83 @@ var docdroid = angular.module('docdroid', ['ionic']);
 
 
 docdroid.controller("HomeMenuCtrl",function($scope,$ionicSideMenuDelegate){
-    
+            /*Initialize Status, this will be stored in database later on*/
+        $scope.docstatus = "Available";
+        $scope.statuscolor = "icon-available";
     $scope.openDrawer = function (){
         $ionicSideMenuDelegate.toggleLeft();
     };
     
     $scope.changeStatus = function(){
+    
         
+        if($scope.docstatus.valueOf() == "Available"){
+            $scope.docstatus = "Away";
+            $scope.statuscolor ="icon-away";
+        }
+        else if ($scope.docstatus.valueOf() == "Away"){
+            $scope.docstatus = "Busy";
+            $scope.statuscolor ="icon-busy";
+        }
+        else if ($scope.docstatus.valueOf() == "Busy"){
+            $scope.docstatus ="Available";
+            $scope.statuscolor ="icon-available";
+        }
     };
+});
+
+docdroid.controller("notificationPopoverCtrl", function($scope,$ionicPopover){
+$ionicPopover.fromTemplateUrl('./templates/notificationPopover.html', {
+    scope: $scope
+  }).then(function(popover) {
+    $scope.popover = popover;
+  });
+    
+$scope.openPopover = function($event) {
+$scope.popover.show($event);
+  };
+  $scope.closePopover = function() {
+    $scope.popover.hide();
+  };
+  //Cleanup the popover when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.popover.remove();
+  });
+  // Execute action on hide popover
+  $scope.$on('popover.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove popover
+  $scope.$on('popover.removed', function() {
+    // Execute action
+  });
+    
+});
+
+docdroid.controller("InboxPopoverCtrl", function($scope,$ionicPopover){
+$ionicPopover.fromTemplateUrl('./templates/inboxPopover.html', {
+    scope: $scope
+  }).then(function(popover) {
+    $scope.popover = popover;
+  });
+    
+$scope.openPopover = function($event) {
+$scope.popover.show($event);
+  };
+  $scope.closePopover = function() {
+    $scope.popover.hide();
+  };
+  //Cleanup the popover when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.popover.remove();
+  });
+  // Execute action on hide popover
+  $scope.$on('popover.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove popover
+  $scope.$on('popover.removed', function() {
+    // Execute action
+  });
+    
 });
